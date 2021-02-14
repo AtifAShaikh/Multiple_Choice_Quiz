af8eef4d2a821000e1348b25f491fa6acf919cd1
AtifAShaikh <atif.arifshaikh25@gmail.com>
2021-02-13 18:40:09 -0800

    finished functionality of game

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var leaderBoardScreen = document.querySelector("#leaderboard-screen");
 // store button references
 var startButton = document.querySelector(".start-button");
 var confirmScoreButton = document.querySelector(".confirm-button");
+var restartButton = document.querySelector(".restart-button");
 
 
 
@@ -14,6 +15,7 @@ var confirmScoreButton = document.querySelector(".confirm-button");
 var timerDisplay = document.querySelector("#time");
 var finalScoreDisplay = document.querySelector(".final-score-display");
 var nameInput = document.querySelector(".name-input");
+var leaderBoardList = document.querySelector(".list-of-scores");
 
 //answer choice div references
 var questionDisplay = document.querySelector(".question-display");
@@ -76,18 +78,45 @@ function startQuiz(){
     displayQuestion(questions[currentQuestion]);
 }
 
+function compare(a,b){
+    var aScore = a.score;
+    var bScore = b.score;
+    if(aScore<bScore){
+        return 1;
+    }
+    if (bScore<aScore){
+        return -1;
+    }
+    return 0;
+}
+
+function displayScores(currentScores){
+    var workingList = Array.from(currentScores);
+    var elementToAdd = document.createElement("li");
+    leaderBoardList.innerHTML = '';
+    workingList.sort(compare);
+    for(var i = 0; i<workingList.length; i++){
+        elementToAdd = document.createElement("li");
+        elementToAdd.textContent = workingList[i].player + ": " + workingList[i].score;
+        leaderBoardList.append(elementToAdd);
+    }
+
+}
+
 function confirmScore(){
  
     scoreScreen.style.display = "none";
     leaderBoardScreen.style.display = "flex";
     currentScores = JSON.parse(localStorage.getItem("scoresList"));
     currentScores.push({player: nameInput.value, score: time});
+    displayScores(currentScores);
     currentScores = JSON.stringify(currentScores);
     localStorage.setItem("scoresList", currentScores);
 }
 
 startButton.onclick = startQuiz;
 confirmScoreButton.onclick = confirmScore;
+restartButton.onclick = function(){location.reload()};
 
 questionBoxes.forEach(element => {
     element.addEventListener("click", function(){
