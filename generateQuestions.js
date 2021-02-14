ef06b7bcc2a79d89d8cad3b23347b776b17f154e
AtifAShaikh <atif.arifshaikh25@gmail.com>
2021-02-14 10:50:48 -0800

    added comments

--- generateQuestions.js
+++ generateQuestions.js
@@ -1,3 +1,6 @@
+// this script creates an array of objects used to store and display the questions
+// this function returns an object that holds the question data
+// first argument is the question, next 4 are the questions and last is which one is the correct answer
 function createQuestionObject(questionInput, choice1Input, choice2Input, choice3Input, choice4Input, correctChoiceInput){
     var correctSet;
     if(correctChoiceInput === 1){
@@ -21,6 +24,8 @@ function createQuestionObject(questionInput, choice1Input, choice2Input, choice3
 
 // list containing the question objects
 var questions = []
+// the function is called various times to create object
+// questions will be displayed in this order
 questions.push(createQuestionObject("Who is considered as the first programmer",
                                     "Ada lovelace",
                                     "Albert einstein",
--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+// READ ME
+// the flow of this script may not seem intuitive at first so this explanation is to shed light on its inner workings
+// the first bit is fairly simple, we are simply adding references to all relevant objects and creating important functional values
+// next we create a local variable scoresList if it does not already exist
+// after this we run into a bunch of even listeners, on click events, and functions
+// the flow is essentially a loop with only the option to progress forwards
+
+// #### Start Screen ####
+// when the website opens it starts with the start screen div open
+// the start screen div has only 1 button to begin the game
+// this button calls the start quiz method
+
+// #### Multiple Choice Screen ####
+// this leads into the multple choice div being activated
+// it has a timer and questions displayed, clicking an answer choice will award time to the timer based on whether or not it is a correct answer
+// then it will display the next question, until it runs out of questions, at whic point it will display the score
+
+// #### Final Score Screen ####
+// the display score will display the left over time as the score and a textinput box to enter the name of the player
+// it also has a confirm box that hides the final score screen and activates the leaderboard div
+
+// #### LeaderBoardScreen ####
+// the leader board div shows the scores on a ul
+// it also has a button to restart which simply refreshes the page bringing us back to the start screen
+
+
+
+
+
+
+
 // store references to the various divs containing the screens
 var startScreen = document.querySelector("#start-screen");
 var multipleChoiceScreen = document.querySelector("#multiple-choice");
@@ -9,15 +40,13 @@ var startButton = document.querySelector(".start-button");
 var confirmScoreButton = document.querySelector(".confirm-button");
 var restartButton = document.querySelector(".restart-button");
 
-
-
-// store various object references
+// store various display object references
 var timerDisplay = document.querySelector("#time");
 var finalScoreDisplay = document.querySelector(".final-score-display");
 var nameInput = document.querySelector(".name-input");
 var leaderBoardList = document.querySelector(".list-of-scores");
 
-//answer choice div references
+// multiple choice div references
 var questionDisplay = document.querySelector(".question-display");
 var questionBoxes = document.querySelectorAll(".answer-choice");
 
@@ -26,15 +55,17 @@ var timerActive = false;
 var time = 0;
 var currentQuestion = 0;
 
+// creates the scoresList local storage if it does not exist yet
 if(localStorage.getItem("scoresList") === null){
-    localStorage.setItem("scoresList", JSON.stringify([]));
-    
+    localStorage.setItem("scoresList", JSON.stringify([])); 
 } 
 
+// displays the time based on time input
 function displayTime(time_input){
     timerDisplay.textContent = time_input;
 }
 
+// begins the timer and runs it until the bool timer active is false
 function startTimer(){
     timerActive = true;
     time = 10;
@@ -50,6 +81,7 @@ function startTimer(){
     1000);
 }
 
+// end the quiz by stopping the timer hiding the multiple choice screen and setting up score screen
 function finishQuiz(){
     timerActive = false;
     multipleChoiceScreen.style.display = "none";
@@ -58,6 +90,8 @@ function finishQuiz(){
     
 }
 
+// handles the displaying of the question and deciding when to finish quiz
+// finishes quiz when the next question to be displayed is undefined, meaning it has run out of questions
 function displayQuestion(question_input){
     if(question_input === undefined){
         finishQuiz();
@@ -71,6 +105,7 @@ function displayQuestion(question_input){
     
 }
 
+// begins the quiz by hiding start screen, showing multiple choice screen, starting timer, then displaying the first question
 function startQuiz(){
     startScreen.style.display = "none";
     multipleChoiceScreen.style.display = "flex";
@@ -78,6 +113,7 @@ function startQuiz(){
     displayQuestion(questions[currentQuestion]);
 }
 
+// comparator used to sort the user scores array
 function compare(a,b){
     var aScore = a.score;
     var bScore = b.score;
@@ -90,6 +126,7 @@ function compare(a,b){
     return 0;
 }
 
+// displays scores on the leaderboard screen, first it sorts the scores from local storage, then appends them all as LI to the leaderboard ul
 function displayScores(currentScores){
     var workingList = Array.from(currentScores);
     var elementToAdd = document.createElement("li");
@@ -103,8 +140,10 @@ function displayScores(currentScores){
 
 }
 
+// activated when user presses confirm score, hides scores screen, open leaderboard screen
+// gets user scores, pushes new score
+// displays he scores, then sets the scores to local storage
 function confirmScore(){
- 
     scoreScreen.style.display = "none";
     leaderBoardScreen.style.display = "flex";
     currentScores = JSON.parse(localStorage.getItem("scoresList"));
@@ -114,10 +153,14 @@ function confirmScore(){
     localStorage.setItem("scoresList", currentScores);
 }
 
+// onclick events set to each button
 startButton.onclick = startQuiz;
 confirmScoreButton.onclick = confirmScore;
+// refreshes the page to restart the quiz
 restartButton.onclick = function(){location.reload()};
 
+// on click event for each question box, determines if time should be added or removed based on if the correct answer was selected
+// then displays the next question
 questionBoxes.forEach(element => {
     element.addEventListener("click", function(){
         if(element.textContent === questions[currentQuestion].correctChoice){
